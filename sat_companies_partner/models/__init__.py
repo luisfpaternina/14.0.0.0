4909a5acc1da5c9ace3c52510cccc21273dbb859
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-01 05:09:47 -0500

    PO

--- sat_companies_partner/models/__init__.py
+++ sat_companies_partner/models/__init__.py
@@ -11,3 +11,4 @@ from . import crm_lead
 from . import product
 from . import account_move
 from . import account_move_line
+from . import purchase_order
---
+++ sat_companies_partner/models/purchase_order.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api, _
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    is_potential_client = fields.Boolean(
+        string="Is a potential client",
+        tracking=True)
