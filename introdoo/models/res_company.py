4ea65c0b6cb7428faa2f9cb2a7be22a1742cbdf7
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-02 06:13:03 -0500

    Update res_company.py

--- introdoo/models/res_company.py
+++ introdoo/models/res_company.py
@@ -21,7 +21,8 @@ class ResCompany(models.Model):
     date2 = fields.Date(
         string="Date 2")
     days = fields.Integer(
-        string="Days")
+        string="Days",
+        compute="_calculate_days")
 
     # Validar que las líneas no se pueda repetir
     @api.constrains('company_line_ids')
@@ -32,3 +33,12 @@ class ResCompany(models.Model):
                 if line.partner_id.id in exis_record_lines:
                     raise ValidationError('No se puede repetir el registro')
                 exis_record_lines.append(line.partner_id.id)
+
+
+    @api.depends('date1','date2')
+    def _calculate_days(self):
+        for rec in self:
+            if rec.date1 and rec.date2:
+                rec.days = (rec.date2 - rec.date1).days
+            else:
+                rec.days = 0
