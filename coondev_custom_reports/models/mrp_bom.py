a1ebc34bad2a699561a4185be6e9eda82f9007ea
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-01 20:06:51 -0500

    Update mrp_bom.py

--- coondev_custom_reports/models/mrp_bom.py
+++ coondev_custom_reports/models/mrp_bom.py
@@ -6,4 +6,13 @@ class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
     total_cost = fields.Float(
-        string="Total")
+        string="Total",
+        compute="_compute_total")
+
+    @api.depends('product_id')
+    def _compute_total(self):
+        for record in self:
+            if record.product_id:
+                record.total_cost = record.product_qty * record.product_id.standard_price
+            else:
+                record.total_cost = 0.0
