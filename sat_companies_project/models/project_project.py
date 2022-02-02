e83e235f15ddfb861f6bd91c3ff0dcdf020fe41c
bitsfernandofernandez <nelson.fernandez@bitsamericas.com>
2022-02-02 16:32:38 -0500

    [UPDATE] Update fixed funtion field_service date

--- sat_companies_project/models/project_project.py
+++ sat_companies_project/models/project_project.py
@@ -12,16 +12,18 @@ class ProjectProject(models.Model):
 
     def update_fields_service_dates(self):
         for record in self:
-            dates_beging = [x.planned_date_begin for x in record.task_ids]
-            date_min = min(dates_beging)
-            diff_dates =record.start_date_project - date_min
-            for task in record.task_ids:
-                if date_min == task.planned_date_begin:
-                    task.planned_date_begin = record.start_date_project
-                    task.planned_date_end = task.planned_date_end + timedelta(days=diff_dates.days)
-                else:
-                    task.planned_date_begin = task.planned_date_begin + timedelta(days=diff_dates.days)
-                    task.planned_date_end = task.planned_date_end + timedelta(days=diff_dates.days)
+            dates_begin = [x.planned_date_begin for x in record.task_ids]
+            if dates_begin[0]:
+                date_min = min(dates_begin)
+                diff_dates =record.start_date_project - date_min
+                for task in record.task_ids:
+                    if task.planned_date_begin and task.planned_date_end:
+                        if date_min == task.planned_date_begin:
+                            task.planned_date_begin = record.start_date_project
+                            task.planned_date_end = task.planned_date_end + timedelta(days=diff_dates.days)
+                        else:
+                            task.planned_date_begin = task.planned_date_begin + timedelta(days=diff_dates.days)
+                            task.planned_date_end = task.planned_date_end + timedelta(days=diff_dates.days)
     
     def write(self,vals):
         update_dates = False
