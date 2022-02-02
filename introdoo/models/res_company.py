82caac6fd10b78be181e0556cfc9703484e70480
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-02 06:00:37 -0500

    Update res_company.py

--- introdoo/models/res_company.py
+++ introdoo/models/res_company.py
@@ -1,17 +1,25 @@
-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    terms = fields.Text(string="Terminos y condiciones")
-    test = fields.Char(string="Test")
-    phone = fields.Char(string="Phone")
+    terms = fields.Text(
+        string="Terminos y condiciones")
+    test = fields.Char(
+        string="Test")
+    phone = fields.Char(
+        string="Phone")
     company_line_ids = fields.One2many(
         'res.company.line',
         'company_id',
         string="Company lines")
+    date1 = fields.Date(
+        string="Date 1")
+    date2 = fields.Date(
+        string="Date 2")
+    days = fields.Integer(
+        string="Days")
 
     @api.constrains('company_line_ids')
     def _exist_record_in_lines(self):
