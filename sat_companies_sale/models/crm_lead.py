610dde732af6333a4d40d8f1b3cc288409ae2884
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-04 06:56:15 -0500

    Update crm_lead.py

--- sat_companies_sale/models/crm_lead.py
+++ sat_companies_sale/models/crm_lead.py
@@ -73,8 +73,3 @@ class CrmLead(models.Model):
                 logging.info(dt_order_ids)
                 logging.info(line.name)
                 logging.info(line.quote_date_sent)
-                if line < dt_order_ids:
-                    dt_order_ids = line.quote_date_sent
-                return dt_order_ids
-                logging.info("RETURNNNNNNNNNNNNNNNNNNNNNNN")
-                logging.info(dt_order_ids)
