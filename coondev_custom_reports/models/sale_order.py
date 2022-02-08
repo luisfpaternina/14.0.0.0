2e8a506d0b21eb7267fb7f1a84a431cb64a24ac8
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-08 07:10:55 -0500

    add

--- coondev_custom_reports/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
