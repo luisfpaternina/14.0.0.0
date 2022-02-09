88cb67377d673cf2e9350bb95e157dc7cb4a892a
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-09 09:42:39 -0500

    Update purchase_order.py

--- sat_companies_purchase/models/purchase_order.py
+++ sat_companies_purchase/models/purchase_order.py
@@ -11,3 +11,13 @@ class PurchaseOrder(models.Model):
         string="Purchase order type")
     is_validate_reception = fields.Boolean(
         string="Validate reception")
+
+
+    @api.onchange('date_planned')
+    def _onchange_date_planned(self):
+        now = datetime.now()
+        for record in self:
+            record.is_validate_reception = False
+            if record.date_planned:
+                if record.date_planned >= now:
+                    record.is_validate_reception = True
