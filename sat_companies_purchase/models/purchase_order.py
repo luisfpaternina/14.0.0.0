24d21cabda8a01023657139721bfc98818e2fbfb
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-09 06:45:54 -0500

    Update purchase_order.py

--- sat_companies_purchase/models/purchase_order.py
+++ sat_companies_purchase/models/purchase_order.py
@@ -2,3 +2,7 @@ from odoo import models, fields, api, _
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    order_type_id = fields.Many2one(
+        'purchase.order.type',
+        string="Purchase order type")
