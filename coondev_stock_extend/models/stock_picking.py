8a1e5064bd82d7291310594eff360fe0f80dcd1d
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-16 11:44:48 -0500

    Update stock_picking.py

--- coondev_stock_extend/models/stock_picking.py
+++ coondev_stock_extend/models/stock_picking.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
+import logging
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -9,9 +10,11 @@ class StockPicking(models.Model):
         related="picking_type_id.is_transfer")
 
 
-    @api.onchange('is_transfer')
+    @api.onchange('is_transfer','picking_type_id')
     def transfer_domain(self):
         for record in self:
             if record.location_dest_id and record.is_transfer:
                 locations = self.env['stock.location'].search([('is_transfer','=', record.is_transfer)])
+                logging.info("?????????????????")
+                logging.info(locations)
                 return {'domain': {'location_dest_id': [('id', 'in', locations)]}}
