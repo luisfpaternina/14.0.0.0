e540006cb04181dd210ac99afb7454098df7a743
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-16 11:37:27 -0500

    Update stock_picking.py

--- coondev_stock_extend/models/stock_picking.py
+++ coondev_stock_extend/models/stock_picking.py
@@ -7,3 +7,11 @@ class StockPicking(models.Model):
     is_transfer = fields.Boolean(
         string="Is transfer",
         related="picking_type_id.is_transfer")
+
+
+    @api.onchange('is_transfer')
+    def transfer_domain(self):
+        for record in self:
+            if record.location_dest_id and record.is_transfer:
+                locations = self.env['stock.location'].search([('is_transfer','=', record.is_transfer)])
+                return {'domain': {'location_dest_id': [('id', 'in', locations)]}}
