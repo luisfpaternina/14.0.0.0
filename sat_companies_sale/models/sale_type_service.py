ec23dda6c44277b6c5ef3cd420523ed1f1a76402
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-17 08:02:37 -0500

    Update sale_type_service.py

--- sat_companies_sale/models/sale_type_service.py
+++ sat_companies_sale/models/sale_type_service.py
@@ -5,8 +5,13 @@ from odoo.exceptions import ValidationError
 class SaleTypeService(models.Model):
     _name = 'sale.type.service'
 
-    name = fields.Char(string="Name")
+    name = fields.Char(
+        string="Name")
     item = fields.Integer()
-    code = fields.Char(string="Code")
-    active = fields.Boolean(string="Active")
-    order_id = fields.Many2one('sale.order', string="Sale Order")
+    code = fields.Char(
+        string="Code")
+    active = fields.Boolean(
+        string="Active")
+    order_id = fields.Many2one(
+        'sale.order',
+        string="Sale Order")
