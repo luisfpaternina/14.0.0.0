d4e93fd79b1b4fd5afb0de22f1e5d7d3497e9874
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-23 14:05:01 -0500

    add

--- coondev_stock_extend/models/stock_location.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-
-class StockLocation(models.Model):
-    _inherit = 'stock.location'
-
-    is_transfer = fields.Boolean(
-        string="Is transfer")
